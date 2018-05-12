846ca653542575046305e1b40ef5dcf9094f41d7
Gerald <geralddzx@gmail.com>
2018-05-11 23:33:55 -0400

    air comments

--- src/Air.lua
+++ src/Air.lua
@@ -1,4 +1,5 @@
--- this layer contains all higher up objects that need to collide and render after the ground
+-- this layer contains all higher up objects 
+-- that need to collide and render after the ground
 Air = Class{}
 
 function Air:init(ground)
@@ -19,7 +20,6 @@ function Air:update(dt)
     for k, object in pairs(self.objects) do
         object:update(dt)
     end
-    -- self:processCollisions()
 end
 
 function Air:render()
@@ -31,16 +31,22 @@ function Air:render()
         drop:render()
     end
 
+    -- sort standing objects by depth, the objects higher in both x and y
+    -- get rendered last because they are closest to perspective
     local depths = {}
     for i = self.startX + self.startY, self.endX + self.endY do
         depths[i] = {}
     end
     
+    -- put object in depth index
+    -- then sort objects in the same index
+    -- this prevents every object having to compare to every other object
     for k, object in pairs(self.objects) do
         table.insert(depths[math.floor(object.x + object.y)], object)
     end
 
     for i = self.startX + self.startY, self.endX + self.endY do
+        -- sort objects by how far they are, render the farther objects first
         local depth = depths[i]
         table.sort(depth, function(farther, closer)
             if farther.x + farther.y < closer.x + closer.y then
@@ -60,16 +66,15 @@ end
 
 function renderObject(object)
     for k, graphic in pairs(object:graphics()) do
+        -- convert isometric coords back to cartesian so love can draw it properly
         love.graphics.draw(graphic[1], graphic[2],
             Cartesian(object.x + object.offsetX, object.y + object.offsetY))
-        -- love.graphics.setColor(255, 255, 255)
-        -- local x, y = Cartesian(object.x, object.y)
-        -- love.graphics.circle("fill", x, y, object.radius * 32) 
     end
 end
 
 function Air:generatePlayer()
-    self.player = Player(1, 1, self)
+    -- player stands on the first tile that get generated
+    self.player = Player(0.5, 0.5, self)
     table.insert(self.objects, self.player)
 
     self.player.stateMachine = StateMachine {
@@ -85,6 +90,7 @@ end
 function Air:generateTrees()
     for y = self.startY, self.endY do
         for x = self.startX, self.endX do
+            -- generate trees on grass
             if IsGrassTile(self.ground.tiles[y][x].id) then
                 if math.random() < 0.05 then
                     table.insert(self.objects, Tree(x - 0.5, y - 0.5))
@@ -97,9 +103,12 @@ end
 function Air:generateMonsters()
     for y = self.startY, self.endY do
         for x = self.startX, self.endX do
+            -- generate monsters only on grass
             if IsGrassTile(self.ground.tiles[y][x].id) then
                 local def = MONSTER_DEFS[math.random(#MONSTER_DEFS)]
                 if (math.random() < 0.05 / def.level ^ 1) then
+                    -- all objects are track by its center, so tile 1's center
+                    -- is 1 - 0.5 = 0.5
                     local monster = Monster(x - 0.5, y - 0.5, def)
                     table.insert(self.objects, monster)
 
@@ -118,6 +127,9 @@ function Air:generateMonsters()
 end
 
 function Air:generateIndex()
+    -- index for tracking all objects that touch each tile,
+    -- this makes it easy because all objects that could collide
+    -- in a certain area is scoped to a few tiles
     self.tiles = {}
     for y = self.startY, self.endY do
         self.tiles[y] = {}
@@ -132,6 +144,7 @@ function Air:generateIndex()
 end
 
 function Air:removeIndex(target)
+    -- remove object from collision tracking index
     for x = math.floor(target.x - target.radius) + 1, math.floor(target.x + target.radius) + 1 do
         for y = math.floor(target.y - target.radius) + 1, math.floor(target.y + target.radius) + 1 do
             for i, object in pairs(self.tiles[y][x]) do
@@ -144,48 +157,26 @@ function Air:removeIndex(target)
 end
 
 function Air:addIndex(target)
-  for x = math.floor(target.x - target.radius) + 1, math.floor(target.x + target.radius) + 1 do
-    for y = math.floor(target.y - target.radius) + 1, math.floor(target.y + target.radius) + 1 do
-        table.insert(self.tiles[y][x], target)
+    -- add object back to collision tracking index
+    for x = math.floor(target.x - target.radius) + 1, math.floor(target.x + target.radius) + 1 do
+        for y = math.floor(target.y - target.radius) + 1, math.floor(target.y + target.radius) + 1 do
+            table.insert(self.tiles[y][x], target)
+        end
     end
-  end
 end
-
--- function Air:processTiles()
---     for y = 1, MAP_SIZE do
---         for x = 1, MAP_SIZE do
---             for k, object in pairs(self.objects) do
---                 for x = math.floor(object.x - object.radius) + 1, math.floor(object.x + object.radius) + 1 do
---                     if not GRASS[self.ground.tiles[y][x].id] then
---                         local dx, dy = object.x - (x - 0.5), object.y - (y - 0.5)
---                         local angle = Angle(x, y)
---                         local hypotenuse = 0.5 * Hypotenuse(angle)
---                         local distance = Magnitude(dx, dy)
---                         local gap = distance - object.radius
-                        
---                         if gap < hypotenuse then
---                             local scaleFactor = (hypotenuse - gap) / distance
---                             object.x = object.x + scaleFactor * dx
---                             object.y = object.y + scaleFactor * dy
---                             object.bumped = true
---                             -- print(self.player.x, self.player.y)
---                         end
---                     end
---                 end
---             end
---         end
---     end
--- end
 
 function Air:processCollisions(object)
     local collided = false
+    -- check all tiles the object spans
     for x = math.floor(object.x - object.radius) + 1, math.floor(object.x + object.radius) + 1 do
         for y = math.floor(object.y - object.radius) + 1, math.floor(object.y + object.radius) + 1 do
+            -- go through each object in each of the spanning tiles
             for k, target in pairs(self.tiles[y][x]) do
                 if self:processCollision(object, target) then
                     collided = true
                 end
             end
+            -- check if the object also collides with a tile that can't be stepped on
             if self:processTileCollision(object, x, y) then
                 collided = true
             end
@@ -194,33 +185,22 @@ function Air:processCollisions(object)
     return collided
 end
 
--- function Air:processCollisions(object)
---   for y = self.startY, self.endY do
---       for x = self.startX, self.endX do
---           local objects = self.tiles[y][x]
---           for i = 1, #objects do
---               for j = i + 1, #objects do
---                   processCollision(objects[i], objects[j])
---               end
---               if not IsGrassTile(self.ground.tiles[y][x].id) then
---                   processCollision(objects[i], self.ground.tiles[y][x])
---               end
---           end
---       end
---   end
--- end
-
 function Air:processCollision(object, target)
+    -- if two objects are exactly in the same place, move 
+    -- the object slightly out of the way
     if object.x == target.x and object.y == target.y then
         object.x = object.x + math.random() * 0.2 - 0.1
         object.y = object.y + math.random() * 0.2 - 0.1
     end
   
+    -- get distance between center of objects
     local x, y = object.x - target.x, object.y - target.y
     local touchDistance = object.radius + target.radius
     local distance = Magnitude(x, y)
   
+    -- if objects are touching
     if distance < touchDistance then
+        -- calculate how much the object should be pushed further in the same direction
         local pushDistance = touchDistance - distance
         local pushScale = pushDistance / distance
         local dx, dy = pushScale * x, pushScale * y
@@ -231,6 +211,7 @@ function Air:processCollision(object, target)
 end
 
 function Air:processTileCollision(object, x, y)
+    -- set object out of non steppable tile based on angle to the tile
     if not self.ground.tiles[y][x].grass then
         local x, y = x - 0.5, y - 0.5
         local angle = Angle(object.x - x, object.y - y)
@@ -246,47 +227,3 @@ function Air:processTileCollision(object, x, y)
         return true         
     end
 end
-
--- function processCollision(a, b)
---     if a.x == b.x and a.y == b.y then
---         a.x = a.x + math.random() * 0.1 * (-1) ^ math.random(2)
---         a.y = a.y + math.random() * 0.1 * (-1) ^ math.random(2)
---     end
-
---     local x, y = b.x - a.x, b.y - a.y
---     local touchDistance = a.radius + b.radius
---     local distance = Magnitude(x, y)
-
---     if distance < touchDistance then
---         a.bumped = true
---         b.bumped = true
-
---         local pushDistance = touchDistance - distance
---         local pushScale = pushDistance / distance
---         local dx, dy = pushScale * x, pushScale * y
-
---         local totalMass = a.mass + b.mass
---         local aPush = b.mass / totalMass
---         local bPush = a.mass / totalMass
-
---         if aPush ~= aPush then
---             aPush = 1
---         end
-
---         if bPush ~= bPush then
---             bPush = 1
---         end
-
---         a.x, a.y = a.x - dx * aPush, a.y - dy * aPush
---         b.x, b.y = b.x + dx * bPush, b.y + dy * bPush
-
---         -- if b.mass > a.mass then
---         --     a.x, a.y = a.x - dx, a.y - dy
---         -- elseif a.mass == b.mass then
---         --     a.x, a.y = a.x - dx * 0.5, a.y - dy * 0.5
---         --     b.x, b.y = b.x + dx * 0.5, b.y + dy * 0.5
---         -- else
---         --     b.x, b.y = b.x + dx, b.y + dy
---         -- end
---     end
--- end
